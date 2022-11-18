3e55165890309902bf6f3ec772e34a1eac5d9165
burlistic <will.webster@cohezion.com.au>
2022-11-18 15:09:25 +0800

    Changed greeting

--- src/HealthStatus.cs
+++ src/HealthStatus.cs
@@ -38,7 +38,7 @@ namespace AzureFunctionAppTemplate.Functions
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, mr {name}. This HTTP triggered function executed successfully!!!.";
+                : $"Hello, {name} my friend. This HTTP triggered function executed successfully!!!.";
 
             return new OkObjectResult(responseMessage);
         }
