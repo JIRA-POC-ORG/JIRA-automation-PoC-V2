a016e66eedb96da8eb2df458a728c5d3a79e174e
Will Webster <will.webster@cohezion.com.au>
2022-11-29 09:39:47 +0800

    Main

--- src/HealthStatus.cs
+++ src/HealthStatus.cs
@@ -38,7 +38,7 @@ namespace AzureFunctionAppTemplate.Functions
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name} my friend. This HTTP triggered function executed successfully!!!. Yay";
+                : $"Hello, {name} my friend. This HTTP triggered function executed successfully!!!. Yay. Testing JIRA Compass";
 
             return new OkObjectResult(responseMessage);
         }
